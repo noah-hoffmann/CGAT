7d6566e33bd7ad98fe800819c58b671022d45798
noah-hoffmann <noahhoffmann1202@gmail.com>
2022-06-08 15:14:50 +0200

    recoded to calculate errors of additional data on random training sets and add to existing csv file

--- Utilities/errors_of_additional_data.py
+++ Utilities/errors_of_additional_data.py
@@ -3,7 +3,6 @@ from CGAT.data import CompositionData
 from torch.utils.data import DataLoader
 import pandas as pd
 import os
-from sklearn.metrics import mean_absolute_error
 import glob
 from get_additional_data import get_composition
 from tqdm import tqdm
@@ -20,22 +19,18 @@ def main():
     data_paths = glob.glob(os.path.join("additional_data", "*", "*.pickle.gz"))
     assert len(data_paths) > 0
     print(f"Found {len(data_paths)} datasets")
-    # sizes = [50_000, 75_000, 100_000, 125_000, 150_000, 200_000, 250_000]
-    # runs = ["f-0_t-2022-01-03_15-07-47",
-    #         "f-0_t-2022-01-09_14-33-04",
-    #         "f-0_t-2022-01-14_14-50-14",
-    #         "f-0_t-2022-01-18_10-00-20",
-    #         "f-0_t-2022-01-22_17-40-56",
-    #         "f-0_t-2022-01-26_22-42-02",
-    #         "f-0_t-2022-02-02_12-39-57"]
-    # model_paths = [glob.glob(os.path.join("tb_logs",
-    #                                       "runs",
-    #                                       "{run}",
-    #                                       "*.ckpt").format(run=run))[0] for run in runs]
-    model_paths = sorted(glob.glob(os.path.join('new_active_learning', 'checkpoints', '*', '*.ckpt')), key=get_seed)
-    seeds = list(map(get_seed, model_paths))
-    df = pd.DataFrame(columns=['comp', 'seed', 'mae'])
-    for i, model_path in zip(seeds, tqdm(model_paths)):
+    sizes = [200_000, 250_000]
+    runs = ["f-0_t-2022-03-02_16-24-35",
+            "f-0_t-2022-03-03_22-20-11",
+            ]
+    model_paths = [glob.glob(os.path.join("tb_logs",
+                                          "runs",
+                                          "{run}",
+                                          "*.ckpt").format(run=run))[0] for run in runs]
+    # model_paths = sorted(glob.glob(os.path.join('new_active_learning', 'checkpoints', '*', '*.ckpt')), key=get_seed)
+    # seeds = list(map(get_seed, model_paths))
+    df = pd.DataFrame(columns=['comp', 'training set size', 'mae'])
+    for i, model_path in zip(sizes, tqdm(model_paths)):
         model = LightningModel.load_from_checkpoint(model_path, train=False)
         model = model.cuda()
 
@@ -46,14 +41,20 @@ def main():
                 max_neighbor_number=model.hparams.max_nbr,
                 target=model.hparams.target
             )
-            loader = DataLoader(dataset, batch_size=1, shuffle=False, collate_fn=collate_fn)
+            loader = DataLoader(dataset, batch_size=500, shuffle=False, collate_fn=collate_fn)
             comp = get_composition(path)
             errors = []
             for batch in loader:
                 _, _, pred, target, _ = model.evaluate(batch)
-                errors.append(mean_absolute_error(target.cpu().numpy(), pred.cpu().numpy()))
-            df.loc[len(df)] = [comp, i, np.mean(errors)]
-        df.to_csv('new_active_learning/errors.csv', index=False)
+                errors.append(np.reshape(np.abs(pred - target), (-1,)))
+            df.loc[len(df)] = [comp, i, np.mean(np.concatenate(errors))]
+        savepath = 'additional_data/random.csv'
+        if not os.path.isfile(savepath):
+            df.to_csv(savepath, index=False)
+        else:
+            old = pd.read_csv(savepath)
+            merged = pd.concat((old, df), ignore_index=True)
+            merged.to_csv(savepath, index=False)
 
 
 if __name__ == '__main__':
